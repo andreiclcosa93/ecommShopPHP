4ffa85d0842b65bb07c816baf26b742652161a97
andrei <36193348+AndreiCosa@users.noreply.github.com>
2022-07-23 17:29:18 +0300

    update Dashboard navbar

--- admin/inc/nav.php
+++ admin/inc/nav.php
@@ -7,29 +7,43 @@
 				<div id="mobnav-btn">Menu <i class="fa fa-bars"></i></div>
 				<ul class="sf-menu">
 					<li>
-						<a href="#">Home</a>
+						<a href="index.php">Dashboard</a>
 					</li>
 					<li>
-						<a href="#">Shop</a>
+						<a href="#">Categories</a>
 						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
 						<ul>
-							<li><a href="#">Category1</a></li>
-							<li><a href="#">Category1</a></li>
-							<li><a href="#">Category1</a></li>
-							<li><a href="#">Category1</a></li>
+							<li><a href="categories.php">View Categories</a></li>
+							<li><a href="addcategory.php">Add Category</a></li>
 						</ul>
 					</li>
+					<li>
+						<a href="#">Products</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="products.php">View products</a></li>
+							<li><a href="addproduct.php">Add Product</a></li>
+						</ul>
+					</li>
+
+					<li>
+						<a href="#">Orders</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="orders.php">View Orders</a></li>
+
+						</ul>
+					</li>
+
 					<li>
 						<a href="#">My Account</a>
 						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
 						<ul>
-							<li><a href="#">My Orders</a></li>
-							<li><a href="#">Logout</a></li>
+							<li><a href="#">Edit profil</a></li>
+							<li><a href="logout.php">Logout</a></li>
 						</ul>
 					</li>
-					<li>
-						<a href="#">Contact</a>
-					</li>
+	
 				</ul>
 				<div class="header-xtra">
 					<div class="s-cart">
